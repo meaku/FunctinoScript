bcb6165e3b918eb4686468d9000f1743858e996d
jhnns <johannes.ewald@peerigon.com>
2012-12-14 15:18:10 +0100

    - Added JS-compiler - Replaced the vm with require.extensions[".fs"] - Fixed some minor bugs

---
+++ examples/helloJavaScript.js
@@ -0,0 +1,9 @@
+"use strict"; // run code in ES5 strict mode
+
+function helloWorld() {
+    console.log("hello functino");
+    console.log("true is ", true);
+    console.log("false is ", false);
+}
+
+helloWorld();
